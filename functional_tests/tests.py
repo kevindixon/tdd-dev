fefe5037a41791cd383bbe4b934f892a8b0bac62
Kevin Dixon <kevinrdixon@gmail.com>
2015-10-23 18:36:53 +0100

    Added FT tests for second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Buy peacock feathers')
 
+        # When hitting enter, taken to a new URL
+        # and now the page lists the item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Thre is still an input box to add an item
@@ -40,4 +44,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+        # Now a new user comes along
+        ## New browser session to make sure no information of previous user
+        ## is coming from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # new user visit home page. There is no sign of previous users list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # New user starts a list with a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # New user gets own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, no trace of previous users list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
